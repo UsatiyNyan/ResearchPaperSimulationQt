e6e76c5f6d3e71e6bf56e6b8ae61524fbffc88f1
Kirill Illarionov <KAIllarionov@sberbank.ru>
2021-01-24 21:50:39 +0300

    additional drawings

--- frontend/simulation_widget/simulation_widget.cpp
+++ frontend/simulation_widget/simulation_widget.cpp
@@ -1,8 +1,6 @@
 #include "simulation_widget.h"
 #include "ui_simulation_widget.h"
 
-#include <memory>
-#include <QPainter>
 #include <QDebug>
 
 SimulationWidget::SimulationWidget(QWidget *parent) :
@@ -23,20 +21,33 @@ void SimulationWidget::paintEvent(QPaintEvent *) {
     auto painter = std::make_unique<QPainter>(this);
     painter->setRenderHint(QPainter::Antialiasing, true);
     painter->save();
+
+    draw_water(painter);
+
     painter->translate(width() / 2, height() / 2);
+
+    draw_ship(painter);
+
     painter->setBrush(Qt::black);
     painter->setPen(QPen(Qt::black, 4));
 
+    auto engine_pts = engine_->points();
+    scale(engine_pts.back());
     std::vector<QPointF> pts;
-    for (auto &p: engine_->points()) {
-        pts.emplace_back(p.x(), p.y());
+    pts.reserve(engine_pts.size());
+    for (auto &p: engine_pts) {
+        pts.emplace_back(p.x() * scale_, p.y() * scale_);
     }
+
+    draw_cells(painter);
 
     painter->translate(-pts.back().x(), -pts.back().y());
     painter->drawPolyline(pts.data(), pts.size());
 
-    emit uv_pose(QString::number(-pts.back().x()),
-                 QString::number(-pts.back().y()));
+    draw_uv(painter);
+
+    emit uv_pose(QString::number(-engine_pts.back().x()),
+                 QString::number(-engine_pts.back().y()));
 
     painter->restore();
 }
@@ -44,4 +55,54 @@ void SimulationWidget::paintEvent(QPaintEvent *) {
 void SimulationWidget::update_engine(const PhysParams &phys_params) {
     engine_ = SimulationEngine{phys_params};
     update();
+}
+
+void SimulationWidget::scale(Point<> reference_point) {
+    double abs_x = std::abs(reference_point.x());
+    double abs_y = std::abs(reference_point.y());
+
+    if (abs_x > abs_y) {
+        scale_ = (width() - 50) / abs_x / 2;
+    } else {
+        scale_ = (height() - 50) / abs_y / 2;
+    }
+}
+
+void SimulationWidget::draw_water(const std::unique_ptr<QPainter> &painter) const {
+    painter->setBrush(Qt::blue);
+    painter->setPen(QPen(Qt::blue, 4));
+    painter->drawLine(0, height() / 2, width(), height() / 2);
+}
+
+void SimulationWidget::draw_ship(const std::unique_ptr<QPainter> &painter) const {
+    painter->setBrush(Qt::transparent);
+    painter->setPen(QPen(Qt::black, 2));
+    QPolygon polygon{};
+    polygon.append(QPoint{-50, 0});
+    polygon.append(QPoint{-80, -40});
+    polygon.append(QPoint{90, -50});
+    polygon.append(QPoint{50, 0});
+    painter->drawPolygon(polygon);
+    polygon.clear();
+    polygon.append(QPoint{-30, -43});
+    polygon.append(QPoint{-25, -60});
+    polygon.append(QPoint{25, -60});
+    polygon.append(QPoint{30, -48});
+    painter->drawPolygon(polygon);
+}
+
+void SimulationWidget::draw_cells(const std::unique_ptr<QPainter> &painter) const {
+
+}
+
+void SimulationWidget::draw_uv(const std::unique_ptr<QPainter> &painter) const {
+    painter->setBrush(Qt::white);
+    painter->setPen(QPen(Qt::black, 3));
+    QPolygon polygon{};
+    polygon.append(QPoint{0, 20});
+    polygon.append(QPoint{40, 0});
+    polygon.append(QPoint{0, -20});
+    polygon.append(QPoint{0, 20});
+    painter->drawPolygon(polygon);
+    painter->drawRoundedRect(QRect{0, -20, 80, 40}, 30, 20);
 }
--- frontend/simulation_widget/simulation_widget.h
+++ frontend/simulation_widget/simulation_widget.h
@@ -2,6 +2,8 @@
 #define SIMULATION_WIDGET_H
 
 #include <QWidget>
+#include <QPainter>
+#include <memory>
 
 #include "simulation_engine.hpp"
 
@@ -30,8 +32,17 @@ class SimulationWidget : public QWidget {
 
  private:
 
+    void scale(Point<> reference_point);
+    void draw_water(const std::unique_ptr<QPainter> &painter) const;
+    void draw_ship(const std::unique_ptr<QPainter> &painter) const;
+    void draw_cells(const std::unique_ptr<QPainter> &painter) const;
+    void draw_uv(const std::unique_ptr<QPainter> &painter) const;
+
+ private:
+
     Ui::SimulationWidget *ui_;
     std::optional<SimulationEngine> engine_{};
+    double scale_{1};
 };
 
 #endif // SIMULATION_WIDGET_H
